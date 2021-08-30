bf47f4b8ce3c61d3160f6a3f68bf9e41b3a937d5
jackzeng <80494702@qq.com>
2021-08-30 14:27:06 +0800

    first commit

---
+++ src/main/java/com/test/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.test.git;
+
+/**
+ * @PackgeName: com.test.git
+ * @ClassName: GitTest
+ * @Author: zengmin
+ * Date: 2021/8/30 14:11
+ * project name: git_test
+ * @Version:
+ * @Description:
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
